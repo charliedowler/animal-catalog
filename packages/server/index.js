f7eb91a16d7b5b53415a973b8f11b4da7cdcffa5
Charlie Dowler <charliedowler@me.com>
2020-07-30 00:00:23 +0100

    comments

--- packages/server/index.js
+++ packages/server/index.js
@@ -11,19 +11,16 @@ const resolvers = {
 }
 
 const server = new ApolloServer({ typeDefs, resolvers });
-// server.listen({
-//   port: 4001
-// }).then(({ url }) => {
-//   console.log(`🚀  Apollo Server ready at ${url}`);
-// });
 
-// For test purposes only, I would not do this in a production app! :)
 const app = express()
 server.applyMiddleware({ app })
+
+// For test purposes only, I would not do this in a production app! :)
 app.use('/reset', (_, res) => {
   store.reset()
   return res.sendStatus(200)
 });
+
 app.listen(4000, () => {
   console.log(`[App]: Listening on port ${4000}`)
 })
